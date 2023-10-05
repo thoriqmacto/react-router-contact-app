e5829ef43554313b77150cd50b66e64ca7f72748
thoriqmacto <thariq.hadad@gmail.com>
2023-10-05 09:06:59 +0400

    Implement react router connecting sidebar link with children element of contact details

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './index.css';
+import Root, {
+  loader as rootLoader,
+  action as rootAction,
+} from './routes/root';
+import ErrorPage from './error-page';
+import Contact, { loader as contactLoader } from './routes/contact';
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <div>Hello world!</div>,
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    loader: rootLoader,
+    action: rootAction,
+    children: [
+      {
+        path: 'contacts/:contactId',
+        element: <Contact />,
+        loader: contactLoader,
+      },
+    ],
   },
 ]);
 
